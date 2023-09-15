73e4bebecf5d5aa946be7cb5b953d8b7df3b0c04
Adam Therrien-Marois <adamletannant@hotmail.ca>
2023-09-15 08:52:07 -0400

    updated to current bd

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -12,11 +12,64 @@ public class Controller : ControllerBase
     public Controller(ILogger<Controller> logger)
     {
         _logger = logger;
+        Table etats_commandes = new Table("etats_commandes",
+            new Column("id", DataType.INT, true),
+            new Column("nom", DataType.VARCHAR),
+            new Column("descriptions", DataType.VARCHAR)
+        );
+        
+        Variable var = new Variable(DataType.VARCHAR, '%');
+        Variable nom = new Variable("nom", DataType.VARCHAR, true);
+        Variable id_espece = new Variable("id_espece", DataType.INT);
+        query = new QueryBuilder(DataType.table)
+            .select(etats_commandes["id"].noAlias, etats_commandes["nom"].noAlias, etats_commandes["descriptions"].noAlias)
+            .from(etats_commandes)
+            .where(
+                new Condition(etats_commandes["nom"], new ParsableValue(DataType.VARCHAR, var.addOperator(OperatorsArithm.ADDITION), nom.addOperator(OperatorsArithm.ADDITION), var.addOperator(OperatorsArithm.ADDITION)), ConditionalOperators.LIKE)
+                //new Condition(animaux["no_espece"], id_espece, ConditionalOperators.EQUALS)
+            )
+            .order(etats_commandes["id"].isAsc)
+            .BuildSelect(false)
+            ;
+        /*
+        queryMain = new QueryBuilder(DataType.table)
+            .select(especes["no"].addAlias("no_espece"), especes["nom"].addAlias("nom_espece"))
+            .from(especes)
+            .BuildSelect(false)
+            ;
+            */
+        executor = new SQLExecutor(connectionString);
     }
 
-    [HttpGet(Name = "GetTest")]
-    public string Get()
+    [HttpGet("GetArray")]
+    public async Task<IActionResult> Get()
     {
-        return "{\"noms\":[\"Adam\", \"AmelieF\", \"ShawnS\", \"Antoine\"],\"no_espece\":1,\"nom_espece\":\"Chien\",\"animaux\":[{\"no\":1,\"nom\":{},\"age\":42},{\"no\":2,\"nom\":{},\"age\":40},{\"no\":3,\"nom\":\"Camomille\",\"age\":41},{\"no\":4,\"nom\":\"Blue\",\"age\":39},{\"no\":5,\"nom\":{},\"age\":40},{\"no\":6,\"nom\":\"Norbert\",\"age\":41},{\"no\":20,\"nom\":\"C\u00E9leste\",\"age\":42},{\"no\":21,\"nom\":\"Silhouette\",\"age\":42},{\"no\":36,\"nom\":\"Raven\",\"age\":40},{\"no\":37,\"nom\":\"Brownie\",\"age\":42},{\"no\":38,\"nom\":\"Blacky\",\"age\":39},{\"no\":39,\"nom\":\"Slate\",\"age\":40},{\"no\":53,\"nom\":\"Rex\",\"age\":42},{\"no\":54,\"nom\":\"Coffee\",\"age\":40},{\"no\":55,\"nom\":\"Cocoa\",\"age\":41},{\"no\":56,\"nom\":\"Cola\",\"age\":41},{\"no\":57,\"nom\":\"Peluche\",\"age\":40},{\"no\":58,\"nom\":{},\"age\":41},{\"no\":59,\"nom\":\"Yin-Yang\",\"age\":39},{\"no\":60,\"nom\":\"Lichette\",\"age\":42},{\"no\":61,\"nom\":\"Dice\",\"age\":41},{\"no\":62,\"nom\":{},\"age\":42}]}";
+        VariableList vars = new VariableList()
+                .Add("nom", "q", DataType.VARCHAR)
+                ;
+        return Ok(await executor.getJSONArray(query, new List<Field>() {
+                    new Field("id"),
+                    new Field("nom"),
+                    new Field("descriptions")
+            }, vars));
+    }
+
+    [HttpGet("GetObject")]
+    public async Task<IActionResult> GetObj()
+    {
+        VariableList vars = new VariableList()
+                .Add("nom", "a", DataType.VARCHAR)
+                ;
+        Dictionary<string, string> binder = new Dictionary<string, string>();
+        binder.Add("id_espece", "no_espece");
+        return Ok(await executor.getJSONObject(queryMain, new List<Field>() { 
+                new Field("no_espece"),
+                new Field("nom_espece"),
+                new FieldDetailed("animaux", query, null, binder, new List<Field>() {
+                    new Field("no"),
+                    new Field("nom"),
+                    new Field("age")
+                })
+            }, null, cancellationToken));
     }
 }
